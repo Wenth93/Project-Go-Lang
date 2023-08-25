e14ae0b8e18e17e7ccc3d4db78b6f829b9d62c26
Wenth93 <64720186+Wenth93@users.noreply.github.com>
2023-08-25 19:42:58 +0200

    Add files via upload

---
+++ handlers/user.go
@@ -0,0 +1,62 @@
+package handlers
+
+import (
+	"net/http"
+
+	"../types"
+
+	"../services"
+	"github.com/labstack/echo/v4"
+)
+
+type UserHandler struct {
+	userService services.UserService
+}
+
+func NewUserHandler(service services.UserService) *UserHandler {
+	return &UserHandler{
+		userService: service,
+	}
+}
+
+func (h *UserHandler) Post(ctx echo.Context) error {
+	var newUser struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	if err := ctx.Bind(&newUser); err != nil {
+		return err
+	}
+
+	user := &types.User{
+		Username: newUser.Username,
+		Password: newUser.Password,
+	}
+
+	// Call the service to create the new user
+	err := h.userService.CreateNewUser(ctx.Request().Context(), user)
+	if err != nil {
+		return err
+	}
+
+	return ctx.JSON(http.StatusCreated, user)
+}
+
+func (h *UserHandler) Login(ctx echo.Context) error {
+	var loginUser struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	if err := ctx.Bind(&loginUser); err != nil {
+		return err
+	}
+
+	token, err := h.userService.Authenticate(ctx.Request().Context(), loginUser.Username, loginUser.Password)
+	if err != nil {
+		return err
+	}
+
+	return ctx.JSON(http.StatusOK, map[string]string{"token": token})
+}
